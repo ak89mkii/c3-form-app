67d144402390dc971f4c102f27aadc1dbe45e0bf
ak89mkii <ak89mkii@gmail.com>
2023-07-24 13:37:17 -0400

    Changed js file and dir names.

---
+++ halo/test.js
@@ -0,0 +1,48 @@
+// Cached Element References:
+
+const oldFileName = document.getElementById("oldName")
+const newFileName = document.getElementById("newName")
+const oldFileNameButton = document.getElementById("oldButton")
+const newFileNameButton = document.getElementById("newButton")
+const oldFileCount = document.getElementById("characterCountOld")
+
+// TESTING!
+const testInput = document.getElementById("goku");
+const testOutput = document.getElementById("vegeta");
+
+// Event Listeners:
+
+// EL-01: Counts number of characters in first input field.
+oldFileName.addEventListener('input', countCharacters);
+
+// oldFileNameButton.addEventListener('click', function() {
+//     convertFileName();
+// })
+
+// newFileName.addEventListener('input', function() {
+//     countCharacters();
+// })
+
+// newFileNameButton.addEventListener('click', function() {
+//     convertFileName();
+// })
+
+// TESTING!
+testInput.addEventListener('input', testCount) 
+
+// Functions:
+
+// Function-01:
+function countCharacters(e) {
+    oldFileCount.textContent = e.target.value.length;
+    // oldFileCount.textContent = e.target.value;
+
+}
+
+// TESTING!
+function testCount(e) {
+    testOutput.textContent =  e.target.value;
+    // oldFileCount.textContent = e.target.value;
+
+}
+
